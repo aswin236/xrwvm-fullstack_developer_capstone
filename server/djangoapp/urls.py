8b2f2e625e22520f471c17c11529302d688fb78c
aswin236 <aswinmuthuadm@gmail.com>
2026-03-06 04:23:43 -0500

    Updated Python files

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,8 +15,16 @@ urlpatterns = [
 
 
     # path for dealer reviews view
+    path('get_dealers', views.get_dealerships, name='get_dealers'),
+    path('get_dealers/<str:state>', views.get_dealerships, name='get_dealers_by_state'),
+    path('dealer/<int:dealer_id>', views.get_dealer_details, name='dealer_details'),
+    path('reviews/dealer/<int:dealer_id>', views.get_dealer_reviews, name='dealer_details'),
+
 
     # path for add a review view
+    path('add_review', views.add_review, name='add_review'),
+
+
     # logout
     path('logout/',views.logout_request, name='logout')
 
